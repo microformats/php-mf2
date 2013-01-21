62aac4cf89ecb4c940ad49bbedfebabafa8cc180
Barnaby Walters <barnaby@waterpigs.co.uk>
2013-01-21 22:10:51 +0000

    Corrected combined µf test datetimes to comply with parsing spec

--- tests/mf2/CombinedMicroformatsTest.php
+++ tests/mf2/CombinedMicroformatsTest.php
@@ -48,8 +48,8 @@ class CombinedMicroformatsTest extends PHPUnit_Framework_TestCase {
     "properties": {
       "name": ["IndieWebCamp 2012"],
       "url": ["http://indiewebcamp.com/2012"],
-      "start": ["2012-06-30T00:00:00+01:00"],
-      "end": ["2012-07-01T00:00:00+01:00"],
+      "start": ["2012-06-30"],
+      "end": ["2012-07-01"],
       "location": [{
         "value": "Geoloqi, 920 SW 3rd Ave. Suite 400, Portland, OR",
         "type": ["h-card"],
