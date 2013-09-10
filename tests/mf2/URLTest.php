fb7de954e333a2acdc17f7e841ec2135f5f27afa
Aaron Parecki <aaron@parecki.com>
2013-09-10 08:13:53 -0700

    adds a couple more tests and fixes test cases for trailing slash

--- tests/mf2/URLTest.php
+++ tests/mf2/URLTest.php
@@ -23,7 +23,7 @@ class URLTest extends PHPUnit_Framework_TestCase {
   public function testReturnsUrlIfAbsolute($assert, $base, $url, $expected) {
     $actual = mf2\resolveUrl($base, $url);
 
-    $this->assertEquals($actual, $expected, $assert);
+    $this->assertEquals($expected, $actual, $assert);
   }
 	
 	public function testData() {
@@ -32,24 +32,40 @@ class URLTest extends PHPUnit_Framework_TestCase {
 		return array(
 			array('Should return absolute URL unchanged',
 				'http://example.com', 'http://example.com', 'http://example.com'),
+
+			array('Should return root given blank path',
+				'http://example.com', '', 'http://example.com/'),
+
+			array('Should return input unchanged given full URL and blank path',
+				'http://example.com/something', '', 'http://example.com/something'),
+
 			array('Should handle blank base URL',
 				'', 'http://example.com', 'http://example.com'),
+
 			array('Should resolve fragment ID',
-				'http://example.com', '#thing', 'http://example.com#thing'),
+				'http://example.com', '#thing', 'http://example.com/#thing'),
+
 			array('Should resolve blank fragment ID',
-				'http://example.com', '#', 'http://example.com#'),
+				'http://example.com', '#', 'http://example.com/#'),
+
 			array('Should resolve same level URL',
 				'http://example.com', 'thing', 'http://example.com/thing'),
+
 			array('Should resolve directory level URL',
 				'http://example.com', './thing', 'http://example.com/thing'),
+
 			array('Should resolve parent level URL at root level',
 				'http://example.com', '../thing', 'http://example.com/thing'),
+
 			array('Should resolve nested URL',
 				'http://example.com/something', 'another', 'http://example.com/something/another'),
+
 			array('Should respect query strings',
 				'http://example.com/index.php?url=http://example.org', '/thing', 'http://example.com/thing'),
+
 			array('Should resolve query strings',
 				'http://example.com/thing', '?stuff=yes', 'http://example.com/thing?stuff=yes'),
+
 			array('Should resolve dir level query strings',
 				'http://example.com', './?thing=yes', 'http://example.com/?thing=yes')
 		);
