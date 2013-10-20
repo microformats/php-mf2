7b3f8bb630ed4d21e6b6160042ac8236d8c713ac
Barnaby Walters <barnaby@waterpigs.co.uk>
2013-10-20 11:53:47 +0000

    Added classic hcard parsing test, stubbed ignore test

--- tests/mf2/ClassicMicroformatsTest.php
+++ tests/mf2/ClassicMicroformatsTest.php
@@ -18,8 +18,18 @@ use DateTime;
  * Mainly based off BC tables on http://microformats.org/wiki/microformats2#v2_vocabularies
  */
 class ClassicMicroformatsTest extends PHPUnit_Framework_TestCase {
-
 	public function setUp() {
 		date_default_timezone_set('Europe/London');
+	}
+	
+	public function testParsesClassicHcard() {
+		$input = '<div class="vcard"><span class="fn n">Barnaby Walters</span></div>';
+		$expected = '{"items": [{"type": ["h-card"], "properties": {"name": ["Barnaby Walters"]}}], "rels": {}}';
+		$parser = new Parser($input);
+		$this->assertJsonStringEqualsJsonString(json_encode($parser->parse()), $expected);
+	}
+	
+	public function testIgnoresClassicClassnamesUnderMf2Root() {
+		
 	}
 }
