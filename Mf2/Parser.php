cde1521837b2ed1ecc80d2a302c6219ff82e654f
Gregor Morrill <git@gregorlove.com>
2014-06-05 11:48:02 -0500

    Fixed implied dt-end for non-value-class mf2. This makes @barnabywalters test "testImpliedDTEndUsingNonValueClassDTStart" pass.

--- Mf2/Parser.php
+++ Mf2/Parser.php
@@ -559,6 +559,10 @@ class Parser {
 			} else {
 				$dtValue = $dt->nodeValue;
 			}
+
+			if ( preg_match('/(\d{4}-\d{2}-\d{2})/', $dtValue, $matches) ) {
+				$dates[] = $matches[0];
+			}
 		}
 
 		/**
