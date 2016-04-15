70cc3c196812bb6fd640bcecf0e698f5cfb9e123
Aaron Parecki <aaron@parecki.com>
2016-04-15 07:58:09 +0200

    add tests to ensure rel values are returned in document order

--- tests/Mf2/RelTest.php
+++ tests/Mf2/RelTest.php
@@ -41,6 +41,51 @@ class RelTest extends PHPUnit_Framework_TestCase {
     $this->assertEquals('http://example.com/webmention', $output['rels']['webmention'][0]);
   }
 
+  public function testRelValueOrder() {
+    $input = '<map><area rel="webmention" href="http://example.com/area"/></map>
+      <a rel="webmention" href="http://example.com/a">webmention me</a>
+      <link rel="webmention" href="http://example.com/link">';
+    $parser = new Parser($input);
+    $output = $parser->parse();
+
+    $this->assertArrayHasKey('webmention', $output['rels']);
+    $this->assertEquals('http://example.com/area', $output['rels']['webmention'][0]);
+    $this->assertEquals('http://example.com/a', $output['rels']['webmention'][1]);
+    $this->assertEquals('http://example.com/link', $output['rels']['webmention'][2]);
+  }
+
+  public function testRelValueOrder2() {
+    $input = '<map><area rel="webmention" href="http://example.com/area"/></map>
+      <link rel="webmention" href="http://example.com/link">
+      <a rel="webmention" href="http://example.com/a">webmention me</a>';
+    $parser = new Parser($input);
+    $output = $parser->parse();
+
+    $this->assertArrayHasKey('webmention', $output['rels']);
+    $this->assertEquals('http://example.com/area', $output['rels']['webmention'][0]);
+    $this->assertEquals('http://example.com/link', $output['rels']['webmention'][1]);
+    $this->assertEquals('http://example.com/a', $output['rels']['webmention'][2]);
+  }
+
+  public function testRelValueOrder3() {
+    $input = '<html>
+      <head>
+        <link rel="webmention" href="http://example.com/link">
+      </head>
+      <body>
+        <a rel="webmention" href="http://example.com/a">webmention me</a>
+        <map><area rel="webmention" href="http://example.com/area"/></map>
+      </body>
+    </html>';
+    $parser = new Parser($input);
+    $output = $parser->parse();
+
+    $this->assertArrayHasKey('webmention', $output['rels']);
+    $this->assertEquals('http://example.com/link', $output['rels']['webmention'][0]);
+    $this->assertEquals('http://example.com/a', $output['rels']['webmention'][1]);
+    $this->assertEquals('http://example.com/area', $output['rels']['webmention'][2]);
+  }
+
   public function testRelValueOnBTag() {
     $input = '<b rel="webmention" href="http://example.com/webmention">this makes no sense</b>';
     $parser = new Parser($input);
