790e0e45fa6e82bc178c359c232faf056cdf58f3
sknebel <sknebel@users.noreply.github.com>
2018-11-19 21:42:21 +0100

    add more complex tests for id parsing

--- tests/Mf2/ParseHtmlIdTest.php
+++ tests/Mf2/ParseHtmlIdTest.php
@@ -19,10 +19,15 @@ class ParseHtmlIdTest extends PHPUnit_Framework_TestCase {
 
 	/** as per https://github.com/microformats/microformats2-parsing/issues/44 */
 	public function testParserIdAttribute() {
-		$test = '<div class="h-feed" id="recentArticles"><h2 class="p-name">Recent Articles</h2><div class="h-entry">Lorem Ipsum</div></div>';
+		$test = '<div class="h-feed" id="recentArticles"><h2 class="p-name">Recent Articles</h2><div class="hentry" id="article">Lorem Ipsum</div>
+		<div class="p-author h-card" id="theAuthor">Max Mustermann</div></div>';
 		$result = Mf2\parse($test);
 		$this->assertArrayHasKey('id', $result['items'][0]);
 		$this->assertEquals('recentArticles', $result['items'][0]['id']);
+		$this->assertArrayHasKey('id', $result['items'][0]['children'][0]);
+		$this->assertEquals('article', $result['items'][0]['children'][0]['id']);
+		$this->assertArrayHasKey('id', $result['items'][0]['properties']['author'][0]);
+		$this->assertEquals('theAuthor', $result['items'][0]['properties']['author'][0]['id']);
 	}
 }
 
