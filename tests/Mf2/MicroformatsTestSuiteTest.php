236e2d92b50800e3e434f0360129612a916810c5
Martijn van der Ven <martijn@vanderven.se>
2020-05-27 10:10:37 +0200

    Split tests per directory, exclude them unless explicitly triggered

--- tests/Mf2/MicroformatsTestSuiteTest.php
+++ tests/Mf2/MicroformatsTestSuiteTest.php
@@ -5,9 +5,36 @@ namespace Mf2\Parser\Test;
 class MicroformatsTestSuiteTest extends \PHPUnit_Framework_TestCase
 {
     /**
-     * @dataProvider htmlAndJsonProvider
+     * @dataProvider mf1TestsProvider
+     * @group microformats/tests/mf1
      */
-    public function testFromTestSuite($input, $expectedOutput)
+    public function testMf1FromTestSuite($input, $expectedOutput)
+    {
+        $parser = new \Mf2\Parser($input, 'http://example.com/');
+        $this->assertEquals(
+            $this->makeComparible(json_decode($expectedOutput, true)),
+            $this->makeComparible(json_decode(json_encode($parser->parse()), true))
+        );
+    }
+
+    /**
+     * @dataProvider mf2TestsProvider
+     * @group microformats/tests/mf2
+     */
+    public function testMf2FromTestSuite($input, $expectedOutput)
+    {
+        $parser = new \Mf2\Parser($input, 'http://example.com/');
+        $this->assertEquals(
+            $this->makeComparible(json_decode($expectedOutput, true)),
+            $this->makeComparible(json_decode(json_encode($parser->parse()), true))
+        );
+    }
+
+    /**
+     * @dataProvider mixedTestsProvider
+     * @group microformats/tests/mixed
+     */
+    public function testMixedFromTestSuite($input, $expectedOutput)
     {
         $parser = new \Mf2\Parser($input, 'http://example.com/');
         $this->assertEquals(
@@ -36,14 +63,16 @@ class MicroformatsTestSuiteTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * Data provider lists all tests from mf2/tests.
+     * Data provider lists all tests from a specific directory in mf2/tests.
      **/
-    public function htmlAndJsonProvider()
+    public function htmlAndJsonProvider($subFolder = '')
     {
+        // Get the actual wanted subfolder.
+        $subFolder = '/mf2/tests/tests' . $subFolder;
         // Ripped out of the test-suite.php code:
         $finder = new \RegexIterator(
             new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator(
-                dirname(__FILE__) . '/../../vendor/mf2/tests/tests',
+                dirname(__FILE__) . '/../../vendor/' . $subFolder,
                 \RecursiveDirectoryIterator::SKIP_DOTS
             )),
             '/^.+\.html$/i',
@@ -53,7 +82,7 @@ class MicroformatsTestSuiteTest extends \PHPUnit_Framework_TestCase
         $tests = array();
         foreach ($finder as $key => $value) {
             $dir = realpath(pathinfo($key, PATHINFO_DIRNAME));
-            $testname = substr($dir, strpos($dir, '/mf2/tests/tests/') + 17) . '/' . pathinfo($key, PATHINFO_FILENAME);
+            $testname = substr($dir, strpos($dir, $subFolder) + strlen($subFolder) + 1) . '/' . pathinfo($key, PATHINFO_FILENAME);
             $test = pathinfo($key, PATHINFO_BASENAME);
             $result = pathinfo($key, PATHINFO_FILENAME) . '.json';
             if (is_file($dir . '/' . $result)) {
@@ -64,5 +93,23 @@ class MicroformatsTestSuiteTest extends \PHPUnit_Framework_TestCase
             }
         }
         return $tests;
+    }
+
+    /**
+     * Following three functions are the actual dataProviders used by the test methods.
+     */
+    public function mf1TestsProvider()
+    {
+        return $this->htmlAndJsonProvider('/microformats-v1');
+    }
+
+    public function mf2TestsProvider()
+    {
+        return $this->htmlAndJsonProvider('/microformats-v2');
+    }
+
+    public function mixedTestsProvider()
+    {
+        return $this->htmlAndJsonProvider('/microformats-mixed');
     }
 }
