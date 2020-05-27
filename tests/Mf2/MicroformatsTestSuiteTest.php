16c2595b519bb09a3f0dcaa8b27b2329ddc50f1c
Martijn van der Ven <martijn@vanderven.se>
2020-05-27 10:09:50 +0200

    Let PHPUnit validate the parser against the mf2/tests suite

---
+++ tests/Mf2/MicroformatsTestSuiteTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Mf2\Parser\Test;
+
+class MicroformatsTestSuiteTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider htmlAndJsonProvider
+     */
+    public function testFromTestSuite($input, $expectedOutput)
+    {
+        $parser = new \Mf2\Parser($input);
+        $this->compareJson(
+            json_decode($expectedOutput, true),
+            $parser->parse()
+        );
+    }
+
+    /**
+     * Objects within JSON are unordered.
+     * Check if all keys from the correct one are present (in any order) in our output.
+     * Then recursively check the contents of those properties.
+     **/
+    public function compareJson($correct, $test)
+    {
+        if (gettype($correct) === 'array'  && $this->isAssoc($correct)) {
+            foreach ($correct as $key => $value) {
+                $this->assertArrayHasKey($key, $test);
+                $this->compareJson($value, $test[$key]);
+            }
+        } else {
+            $this->assertEquals($correct, $test);
+        }
+    }
+
+    /**
+     * Check if the encountered array is an associative array (has string keys).
+     * @see https://stackoverflow.com/a/173479
+     **/
+    public function isAssoc($array)
+    {
+        return array() !== $array && array_keys($array) !== range(0, count($array) - 1);
+    }
+
+    /**
+     * Data provider lists all tests from mf2/tests.
+     **/
+    public function htmlAndJsonProvider()
+    {
+        // Ripped out of the test-suite.php code:
+        $finder = new \RegexIterator(
+            new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator(
+                dirname(__FILE__) . '/../../vendor/mf2/tests/tests',
+                \RecursiveDirectoryIterator::SKIP_DOTS
+            )),
+            '/^.+\.html$/i',
+            \RecursiveRegexIterator::GET_MATCH
+        );
+        // Build the array of separate tests:
+        $tests = array();
+        foreach ($finder as $key => $value) {
+            $dir = realpath(pathinfo($key, PATHINFO_DIRNAME));
+            $test = pathinfo($key, PATHINFO_BASENAME);
+            $result = pathinfo($key, PATHINFO_FILENAME) . '.json';
+            if (is_file($dir . '/' . $result)) {
+                $tests[pathinfo($key, PATHINFO_FILENAME)] = array(
+                    'input' => file_get_contents($dir . '/' . $test),
+                    'expectedOutput' => file_get_contents($dir . '/' . $result)
+                );
+            }
+        }
+        return $tests;
+    }
+}
